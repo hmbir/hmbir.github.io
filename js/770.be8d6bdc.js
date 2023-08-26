581d905047737e599459d539b8ab2370df8b2448
hmbir <131582140+hmbir@users.noreply.github.com>
2023-08-26 16:25:42 +0800

    Delete js directory

--- js/770.be8d6bdc.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkir"]=self["webpackChunkir"]||[]).push([[770],{7770:function(t,r,s){s.r(r),s.d(r,{default:function(){return c}});var e=function(){var t=this,r=t._self._c;return r("div",{staticClass:"index"},[r("div",{staticClass:"header"}),r("div",{staticClass:"content"},[r("router-link",{attrs:{to:"bar"}},[t._v("条形")]),r("router-link",{attrs:{to:"pie"}},[t._v("扇形")])],1)])},n=[],i={name:"index",props:{msg:String},data(){return{}},methods:{}},a=i,u=s(1001),l=(0,u.Z)(a,e,n,!1,null,"7039776c",null),c=l.exports}}]);
-//# sourceMappingURL=770.be8d6bdc.js.map
